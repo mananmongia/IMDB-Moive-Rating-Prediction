afe8063e95b7e5b2ec2f18856902957cffbd2e02
Manan Mongia <mongiam4@gmail.com>
2019-07-05 22:34:51 +0530

    imdb

---
+++ clean_review.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Create NLP pipeline to Clean Data
+# 
+#     - Load Input and read Reviews
+#     - Tokenize 
+#     - Remove stop words 
+#     - Perform Stemimg
+#     - Write Clean Data to output file
+
+# In[1]:
+
+
+sample_text = "I loved this movie since I was 7 and I saw it on the opening day. It was so touching and beautiful. I strongly recommend seeing for all. It's a movie to watch with your family by far.<br /><br />My MPAA rating: PG-13 for thematic elements, prolonged scenes of disastor, nudity/sexuality and some language."
+
+
+# # NLTK
+#     - for a single review string
+
+# In[2]:
+
+
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+
+import system as sys
+
+
+# In[3]:
+
+
+tokenizer = RegexpTokenizer(r'\w+')
+# import nltk
+# nltk.download('stopwords')
+en_stopwords = set(stopwords.words('english'))
+ps = PorterStemmer()
+
+
+# In[4]:
+
+
+def getStemmedReview(review):
+    review = review.lower()
+    review = review.replace("<br /><br />"," ")
+    
+    #Tokenize
+    tokens = tokenizer.tokenize(review)
+    new_tokens = [token for token in tokens if token not in en_stopwords]
+    stemmed_token = [ps.stem(token) for token in new_tokens]
+    
+    cleanned_review = ' '.join(stemmed_token)
+    return cleanned_review
+
+
+# In[5]:
+
+
+getStemmedReview(sample_text)
+
+
+# #### Function to accept input file and returns output file of clean reviews of moive
+
+# In[6]:
+
+
+def getStemmedDocument(inputFile, outputFile):
+    with open(inputFile, encoding='utf8') as f :
+        reviews = f.readlines()
+        
+    out = open(outputFile,'w', encoding='utf8')
+    
+    for review in reviews:
+        cleanned_review = getStemmedReview(review)
+        print((cleanned_review), file=out)
+        
+    out.close()
+
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
+getStemmedDocument(inputFile,outputFile)
+
